cecf4d494d8b72d4842a4ec849c864b48eeb6124
juacomiranda <jmiranda@live.cl>
2023-09-12 03:16:00 +0000

    navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -6,7 +6,7 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar bg-danger border-bottom border-body">
   			<div className="container">
-    			<a className="navbar-brand" href="#">
+    			<a className="navbar-brand link-light" href="#">
       				<img src="https://www.clarochile.cl/portal/cl/recursos_tema/assets/vector/logo-claro-blanco.svg" alt="Logo" className="d-inline-block align-text-top me-4"></img>
       					Distribuidor Autorizado
    				</a>
