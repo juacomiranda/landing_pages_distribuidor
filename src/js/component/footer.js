5d6858963e6d0318eb04c2dab878d36c86d6de9a
juacomiranda <jmiranda@live.cl>
2023-10-02 16:35:14 +0000

    footer2

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -4,10 +4,10 @@ import logo from "../../img/LOGO_GUEMAC_PNG.png";
 import "../../styles/home.css";
 
 export const Footer = () => (
-  <footer className="footer py-4 text-centerd ">
+  <footer className="footer py-3 text-center">
     <img src={logo} className="float-start ms-5" />
     <p>
-      <a href="http://www.pymeclarochile.cl">Guemac Distribuidor Autorizado</a>
+      <a href="http://www.pymeclarochile.cl">Distribuidor Autorizado</a>
     </p>
   </footer>
 );
